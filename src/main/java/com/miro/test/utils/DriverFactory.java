f7846820e8289d1f0f34f385680e1643020745b1
Akshat <akshat55narang@gmail.com>
2022-07-23 16:29:00 +0200

    setup DriverFactory to supply browser type and configuration

---
+++ src/main/java/com/miro/test/utils/DriverFactory.java
@@ -0,0 +1,53 @@
+package com.miro.test.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static com.miro.test.configs.ConfigManager.isBrowserHeadless;
+
+public class DriverFactory {
+
+    public static WebDriver getDriver(Browser browser) {
+        switch (browser) {
+            case CHROME: {
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver(getDefaultChromeOptions());
+            }
+            case FIREFOX: {
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            }
+            default: {
+                throw new RuntimeException("Brower" + browser + "not supported!!");
+            }
+        }
+    }
+
+    public enum Browser {
+        CHROME,
+        FIREFOX
+    }
+
+    private static ChromeOptions getDefaultChromeOptions() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments(
+                "--disable-popup-blocking",
+                "--ignore-certificate-errors",
+                "--disable-default-apps",
+                "--enable-logging",
+                "--ssl-protocol=tlsv1.2",
+                "--webdriver-loglevel=info"
+        );
+
+        if (!isBrowserHeadless())
+        chromeOptions.addArguments(
+                "--headless"
+        );
+        return chromeOptions;
+    }
+
+}
+
